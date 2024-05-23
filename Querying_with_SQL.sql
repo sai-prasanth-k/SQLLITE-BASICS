cd689ba97cbbd91a3b0bd315d7f81d97a8aa41dc
Sai Prasanth K <93113585+sai-prasanth-k@users.noreply.github.com>
2024-05-23 13:36:05 +0530

    Practices of Query Operations
    Query operations like "LIKE", "<","<=", ">", ">=", "=", "<>" are practiced with the help of random questionnaires from online

---
+++ Querying_with_SQL.sql
@@ -0,0 +1,44 @@
+SELECT * FROM product WHERE price <= 1000;
+
+SELECT
+  *
+FROM
+  product
+WHERE
+  rating > 4.0;
+
+SELECT
+  *
+FROM
+  product
+WHERE
+  brand LIKE "Puma";
+
+SELECT
+  *
+FROM
+  product
+WHERE
+  name LIKE "%Jeans";
+
+SELECT
+  *
+FROM
+  product
+WHERE
+  name LIKE "Bourbon%";
+
+SELECT
+  *
+FROM
+  product
+WHERE
+  name LIKE "%Chips%";
+
+SELECT
+  *
+FROM
+  product
+WHERE
+  name LIKE "%h_r_";
+
